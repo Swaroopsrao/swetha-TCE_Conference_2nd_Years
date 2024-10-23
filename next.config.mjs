eda4e707931a74fa523af45e5749cda7203c377b
Swetha701 <mswetha@student.tce.edu>
2024-10-23 19:11:57 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+  basePath: "/TCEResearchConclave",
+};
+
+export default nextConfig;
